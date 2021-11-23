050aba5decaeb87d5fe8c6d0095d9392e15c967b
justicsc <82329123+justicsc@users.noreply.github.com>
2021-11-22 19:26:29 -0500

    Create Twitter.h

---
+++ Twitter.h
@@ -0,0 +1,17 @@
+#ifndef Twitter_h
+#define Twitter_h
+
+#include"SocialMediaAccount.h"
+template<class T>
+class Twitter:public SocialMediaAccount<T>{
+
+public:
+    Twitter();
+    Twitter(T user);
+    void RT();
+private:
+    int retweetCount;
+
+};
+
+#endif
