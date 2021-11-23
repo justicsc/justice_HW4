da5265289bbd329f4ecd75230f475d531a58f5f5
justicsc <82329123+justicsc@users.noreply.github.com>
2021-11-22 22:32:47 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 #include"SocialMediaAccount.cpp"
 #include"Twitter.cpp"
 
+
+//https://github.com/justicsc/justice_HW4/tree/main
+
 using namespace std;
 
 struct Profile
@@ -20,6 +23,7 @@ int main()
     Twitter<Profile> TP;
     Twitter<string> TS2("John");
     Twitter<Profile> Tp2(start);
+
 
 
 }
