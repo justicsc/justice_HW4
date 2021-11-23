01dea1f4352cba5707c4945557133922ff5d132c
justicsc <82329123+justicsc@users.noreply.github.com>
2021-11-22 19:25:40 -0500

    Create SocialMediaAccount.h

---
+++ SocialMediaAccount.h
@@ -0,0 +1,31 @@
+#ifndef Social
+#define Social
+const int maxFollowers=10;
+const int maxFollowed=10;
+template<class T>
+class SocialMediaAccount{
+
+public:
+    SocialMediaAccount();
+    SocialMediaAccount(T user);
+private:
+    T handler;
+    int followerCount, followedCount;
+    T followers[maxFollowers];
+    T followed[maxFollowed];
+    bool privacy;
+
+
+
+};
+
+#endif
+
+/*
+
+• T handler (this represents the account owner)
+• T followers[maxFollowers ]
+• T followed[ maxFollowed]
+• int followerCount to keep track of your current number of followers.
+• int followedCount to keep track of your current number of followed accounts.
+*/
