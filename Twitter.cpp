6e53531394f1296d51b93006b20a8e3172fb73af
justicsc <82329123+justicsc@users.noreply.github.com>
2021-11-22 22:32:19 -0500

    Add files via upload

--- Twitter.cpp
+++ Twitter.cpp
@@ -4,3 +4,23 @@ Twitter<T>::Twitter():SocialMediaAccount<T>(){}
 
 template<class T>
 Twitter<T>::Twitter(T user):SocialMediaAccount<T>(user){}
+
+
+template<class T>
+int Twitter<T>::getRetweetCount(){
+    return retweetCount;
+
+};
+
+template<class T>
+int Twitter<T>::setRetweetCount(int RTCO){
+    retweetCount = RTCO;
+    return RTCO;
+
+};
+
+template<class T>
+        void Twitter<T>::RT(){
+                 retweetCount++;
+
+};
