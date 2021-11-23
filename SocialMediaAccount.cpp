811a8cbf8294660b84f8cdd5210e8a01c28c3122
justicsc <82329123+justicsc@users.noreply.github.com>
2021-11-22 21:26:59 -0500

    Add files via upload

--- SocialMediaAccount.cpp
+++ SocialMediaAccount.cpp
@@ -1,3 +1,4 @@
+
 #include "SocialMediaAccount.h"
 
 template<class T>
@@ -6,3 +7,25 @@ SocialMediaAccount<T>::SocialMediaAccount(): followerCount(0), followedCount(0),
 
 template<class T>
 SocialMediaAccount<T>::SocialMediaAccount(T user):handler(user), followerCount(0), followedCount(0), privacy(false){}
+
+template<class T>
+int SocialMediaAccount<T>::getFollowerCount(){
+    return followerCount;
+
+};
+template<class T>
+int SocialMediaAccount<T>::getFollowedCount(){
+    return followedCount;
+};
+template<class T>
+int SocialMediaAccount<T>::setFollowerCount(int follCount){
+    int followerCount = follCount;
+    return follCount;
+};
+template<class T>
+int SocialMediaAccount<T>::setFollowedCount(int foledCount){
+    int followedCount = foledCount;
+    return foledCount;
+
+};
+
